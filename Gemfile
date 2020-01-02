d3b97d48aa6c7c9c7752dc32d020ef9f22b90a94
nolan295 <nolan796@live.ca>
2020-01-02 21:25:05 +0000

    initial commit

--- Gemfile
+++ Gemfile
@@ -45,3 +45,6 @@ group :development do
   gem 'spring'
 end
 
+gem 'rails_12factor', group: :production
+
+ruby "2.3.5"
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require_tree
 //= require turbolinks
-//= require_tree .
+
