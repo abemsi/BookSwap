59a36be67fc6ec80c3f0188086c364b455ea9fbf
nolan295 <nolan796@live.ca>
2020-01-04 21:23:01 +0000

    tmp logs

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,13 @@ ActiveRecord::Schema.define(version: 20200103175440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "chats", force: :cascade do |t|
+    t.text     "message"
+    t.string   "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
