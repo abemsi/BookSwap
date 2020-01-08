dc83f9c1e523c90d1a001b91d7e3e725253a27ed
Amy Bemister <amy.bemister@gmail.com>
2020-01-08 20:30:21 +0000

    Add table and migration for pg_search

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200106171730) do
+ActiveRecord::Schema.define(version: 20200108193818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,16 +28,21 @@ ActiveRecord::Schema.define(version: 20200106171730) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "chats", force: :cascade do |t|
-    t.text     "message"
-    t.string   "username"
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "genres", force: :cascade do |t|
-    t.string "name"
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
+
+  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
