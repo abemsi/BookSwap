f63abb8fc31c5efb807a5cdb09b652b1e78672f8
nolan295 <nolan796@live.ca>
2020-01-08 20:51:49 +0000

    Add 'genre' and 'rating' to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200106171730) do
+ActiveRecord::Schema.define(version: 20200108185548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20200106171730) do
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
+    t.text   "genre"
+    t.string   "rating"
     t.text     "description"
     t.string   "image"
     t.text     "wishlist"
