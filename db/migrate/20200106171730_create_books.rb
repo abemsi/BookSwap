318237c71b6b6bdf0af17a904dc33a7dd8c9da4f
nolan295 <nolan796@live.ca>
2020-01-09 00:56:26 +0000

    Add necessary migration fields for incoming data

--- db/migrate/20200106171730_create_books.rb
+++ db/migrate/20200106171730_create_books.rb
@@ -1,13 +1,17 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.string :title
-      t.string :author
-      t.text :description
-      t.string :image
-      t.text :wishlist
-      t.string :condition
-      t.string :location
+      t.string   :title
+      t.string   :author
+      t.text     :description
+      t.string   :image
+      t.text     :wishlist
+      t.text     :rating
+      t.string   :condition
+      t.string   :location
+      t.text     :publisher
+      t.text     :pub_date
+      t.text     :category
 
       t.timestamps null: false
     end
