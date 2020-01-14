9be133ee379ffd16f35ac5cde5d23263611849b8
Amy Bemister <amy.bemister@gmail.com>
2020-01-14 01:35:33 +0000

    Add @ before user in create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    @user = User.find_by_email(params[:email])
 
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/login'
