db980ff1f205f2bfa793971959eb7cdb9700981c
Amy Bemister <amy.bemister@gmail.com>
2020-01-14 01:36:13 +0000

    Add @user to new action, update create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def index 
@@ -8,13 +9,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+       
+      render :new
     end
   end
 
