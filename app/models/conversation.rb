ab1f27dedcc6e50efe0f42d090dd6bb0dd92f880
Amy Bemister <amy.bemister@gmail.com>
2020-01-14 01:30:28 +0000

    Add validations, update between scope

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,12 +1,15 @@
 class Conversation < ActiveRecord::Base
   belongs_to :sender, foreign_key: :sender_id, class_name: "User"
-  belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
+  belongs_to :book, foreign_key: :book_id, class_name: "Book"
+
 
   has_many :messages
 
-  validates_uniqueness_of :sender_id, scope: :recipient_id
+  validates_uniqueness_of :sender_id, scope: :book_id
+  validates :sender_id, :book_id, presence: true
 
-  scope :between, -> (sender_id, recipient_id) do
-    where("(conversations.sender_id = ? AND conversations.recipient_id = ?) OR (conversations.sender_id = ? AND conversations.recipient_id = ?)", sender_id, recipient_id, recipient_id, sender_id)
+  scope :between, -> (sender_id, book_id) do
+    where("(conversations.sender_id = ? AND conversations.book_id = ?) OR (conversations.sender_id = ? AND conversations.book_id = ?)", sender_id, book_id, book_id, sender_id)
   end
+
 end
