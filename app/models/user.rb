7446d5eb5eaa8afc28161c800e3f4ee64e2cb881
Amy Bemister <amy.bemister@gmail.com>
2020-01-14 01:33:13 +0000

    Add validations for sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :books
+
+  validates :name, :email, :password_digest, presence: true
+  validates :email, uniqueness: true, case_sensitive: false
+  validates :password, length: { minimum: 6 }
 end
