3dc7be0c182b30c8c2eaca821c5f75c389d28f00
nolan295 <nolan796@live.ca>
2020-01-14 02:14:27 +0000

    Fix upload image button

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,10 @@
 
 User.create(name: "Amy", email: "amy.bemister@gmail.com", password: "password")
 User.create(name: "Joel", email: "joel@gmail.com", password: "password")
+User.create(name: "david", email: "david@gmail.com", password: "password")
+User.create(name: "jacob", email: "jacob@gmail.com", password: "password")
+User.create(name: "riley", email: "riley@gmail.com", password: "password")
+User.create(name: "alyshah", email: "alyshah@gmail.com", password: "password")
+User.create(name: "mom", email: "mom@gmail.com", password: "password")
+User.create(name: "dad", email: "dad@gmail.com", password: "password")
+
