b536a28cc08a428c06527d6f58de24e262e62fd3
Amy Bemister <amy.bemister@gmail.com>
2020-01-14 01:33:40 +0000

    Add book relation to conversations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
-  belongs_to :genre
   belongs_to :user
+  has_many :conversations
 
   include PgSearch
   pg_search_scope :search, against: [:title, :author, :category],
