77e908b09140e6dcd6d55bfd80217fc9effa05da
Amy Bemister <amy.bemister@gmail.com>
2020-01-14 01:44:58 +0000

    Remove recipient_id and add book_id to conversations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200110055117) do
+ActiveRecord::Schema.define(version: 20200113223236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,9 +45,9 @@ ActiveRecord::Schema.define(version: 20200110055117) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
-    t.integer  "recipient_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "book_id"
   end
 
   create_table "genres", force: :cascade do |t|
@@ -79,9 +79,11 @@ ActiveRecord::Schema.define(version: 20200110055117) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "postal_code"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
 
   add_foreign_key "books", "users"
+  add_foreign_key "conversations", "books"
 end
